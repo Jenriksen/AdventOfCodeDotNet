8edd0df61af57f79a92fa3cd6366762d20d1db6a
Jesper Henriksen <jesperhen89@gmail.com>
2023-10-08 23:25:09 +0200

    wip

--- AdventOfCode/2015/Day05.cs
+++ AdventOfCode/2015/Day05.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using AdventOfCodeSupport;
 
 namespace AdventOfCode._2015;
@@ -7,6 +8,36 @@ public class Day05 : AdventBase
     protected override void InternalPart1()
     {
         Console.WriteLine("Day 5 - Part 1: ");
+
+        int niceStrings = 0;
+
+        bool containsAtLeast3Wovels = false;
+        
+        // regex will kill me...
+        string disallowedPattern = @"ab|cd|pq|xy";
+        
+        // first rule
+        string containsAtLeast3WovelsPattern = @"[aeiouy]{3,}";
+        
+        // second rule
+        string duplicateLettersPattern = @"(.)\1+";
+
+        foreach (string line in InputLines)
+        {
+            if (Regex.IsMatch(line, containsAtLeast3WovelsPattern))
+            {
+                if (Regex.IsMatch(line, duplicateLettersPattern))
+                {
+                    if (!Regex.IsMatch(line, disallowedPattern))
+                    {
+                        niceStrings++;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"Amount of nice strings is: {niceStrings}");
+
     }
 
     protected override void InternalPart2()
